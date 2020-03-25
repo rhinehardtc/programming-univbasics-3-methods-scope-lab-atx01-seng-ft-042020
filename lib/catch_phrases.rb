ef65a735e8397e746911b37aaeac0aff4392e46d
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-03-25 22:01:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,6 +14,15 @@ end
 
 #
 def link
-  phrase = "Squeewoo"
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
 end
 #
+
+#
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
+#  
